4fcf32d3365d838370677cd56c360c73446fd6eb
Julia Guerreiro <juguerreiro123@gmail.com>
2022-03-04 14:27:51 -0300

    view new created

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,11 +1,26 @@
 class PlantsController < ApplicationController
-  
+
   def index
    #@plants = policy_scope(Plant).order(created_at: :desc)
    @plants = Plant.all
   end
-  
+
   def show
     @plant = Plant.find(params[:id])
+  end
+
+  def new
+    @plant = Plant.new
+  end
+
+  def create
+    @plant = Plant.new(plant_params)
+    @plant.save
+  end
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(:specie, :price, :description, :quantity, :is_avaiable)
   end
 end
