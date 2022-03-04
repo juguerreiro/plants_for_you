bd590b7c5811a7267e6b33dcdb0398bffd817bc8
Julia Guerreiro <juguerreiro123@gmail.com>
2022-03-04 15:37:45 -0300

    create a view new for plants

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -15,12 +15,14 @@ class PlantsController < ApplicationController
 
   def create
     @plant = Plant.new(plant_params)
+    @plant.user = current_user
     @plant.save
+    redirect_to plants_path
   end
 
   private
 
   def plant_params
-    params.require(:plant).permit(:specie, :price, :description, :quantity, :is_avaiable)
+    params.require(:plant).permit(:specie, :price, :description, :quantity, :is_avaiable,:photo)
   end
 end
