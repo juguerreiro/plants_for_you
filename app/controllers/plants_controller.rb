74d37596036415d28cd1f343c4c5098360ad78b2
Mariana Lima <marianalima.amb@gmail.com>
2022-03-04 14:02:27 -0300

    policy and pundit

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,7 +1,7 @@
 class PlantsController < ApplicationController
   
   def index
-   #@plants = policy_scope(Plant).order(created_at: :desc)
+   @plants = policy_scope(Plant).order(created_at: :desc)
    @plants = Plant.all
   end
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_02_200604) do
+ActiveRecord::Schema.define(version: 2022_03_04_162027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,9 +42,9 @@ ActiveRecord::Schema.define(version: 2022_03_02_200604) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.boolean "is_seller"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
